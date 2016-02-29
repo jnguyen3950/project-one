19008800201a206ac22edfd8eb3460e8d1d54be0
Justin Nguyen <jnguyen3950@gmail.com>
2016-02-29 14:23:21 -0800

    Media is now generated from javascript file. #3

--- js/default.js
+++ js/default.js
@@ -2,7 +2,8 @@ var myPlaces = [
   {
     placeName: "Hometown",
     star: 4,
-    info: "Vestibulum ante vulputate parturient lacinia odio quam parturient et fringilla ac tempor parturient ultricies."
+    info: "Vestibulum ante vulputate parturient lacinia odio quam parturient et fringilla ac tempor parturient ultricies.",
+    refLink: 'Restaurant.html'
   },
   {
     placeName: "Shrimp",
@@ -21,6 +22,60 @@ var myPlaces = [
   }
 ];
 
+
+//
+// //Star block
+// for (var i = 1; i <= myPlaces[0].star; i++) {
+//   var position = document.getElementById('reviewStar'+i);
+//   position.setAttribute("class", "fa fa-star");
+// }
+// //End Star block
+//
+
+
+
+//Generating Media block
+var testPlace = document.createElement('div');
+testPlace.setAttribute("class", "col-md-8 col-md-offset-2");
+
+var testMedia = document.createElement('div');
+testMedia.setAttribute("class", "media");
+
+var testMediaHeading = document.createElement('div');
+testMediaHeading.setAttribute("class", "media-left media-middle");
+
+var testLink = document.createElement('a');
+testLink.setAttribute("href", "restaurant.html");
+
+var testImg = document.createElement('img');
+testImg.setAttribute("class", "media-object");
+testImg.setAttribute("src", "images/yelp-logo.png");
+
+testLink.appendChild(testImg);
+testMediaHeading.appendChild(testLink);
+testMedia.appendChild(testMediaHeading);
+
+var testMediaBody = document.createElement('div');
+testMediaBody.setAttribute("class", "media-body");
+
+var testH3 = document.createElement('h3');
+testH3.setAttribute("class", "media-heading");
+testH3.setAttribute("id", "place");
+
+var testP = document.createElement('p');
+testP.setAttribute("id", "review1");
+
+testMediaBody.appendChild(testH3);
+testMediaBody.appendChild(testP);
+
+testMedia.appendChild(testMediaBody);
+
+testPlace.appendChild(testMedia);
+//End generating media block
+
+position = document.getElementById('mediaBlock');
+position.appendChild(testMedia);
+
 //Place name block
 var place = document.getElementById('place');
 var elPlace = document.createElement('span');
@@ -28,14 +83,7 @@ var placeText = document.createTextNode(myPlaces[0].placeName);
 
 elPlace.appendChild(placeText);
 place.appendChild(elPlace);
-//End place name
-
-//Star block
-for (var i = 1; i <= myPlaces[0].star; i++) {
-  var position = document.getElementById('reviewStar'+i);
-  position.setAttribute("class", "fa fa-star");
-}
-//End Star block
+// //End place name
 
 // Review block
 var inputReview = document.getElementById('review1');
@@ -44,13 +92,6 @@ var elText = document.createTextNode(myPlaces[0].info);
 
 el.appendChild(elText);
 inputReview.appendChild(el);
-//End Review block
+// End Review block
 
-var testPlace = document.createElement('div');
-testPlace.setAttribute("class", "col-md-8 col-md-offset-2");
-
-var testPanel = document.createElement('div');
-testPanel.setAttribute("class", "panel panel-default");
-
-var testPanelHeading = document.createElement('div');
-testPanelHeading.setAttribute("class", "panel-heading");
+console.log(testPlace);
