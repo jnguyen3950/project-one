96a4c5a865fe96cc5e13926a61dabeda1ebdfde8
Justin Nguyen <jnguyen3950@gmail.com>
2016-02-29 15:03:46 -0800

    Javascript will now iterate through variable object and create media blocks accordingly.

--- js/default.js
+++ js/default.js
@@ -22,76 +22,84 @@ var myPlaces = [
   }
 ];
 
-
-//
-// //Star block
-// for (var i = 1; i <= myPlaces[0].star; i++) {
-//   var position = document.getElementById('reviewStar'+i);
-//   position.setAttribute("class", "fa fa-star");
-// }
-// //End Star block
-//
-
-
-
 //Generating Media block
-var testPlace = document.createElement('div');
-testPlace.setAttribute("class", "col-md-8 col-md-offset-2");
+var mediaPlace = document.createElement('div');
+mediaPlace.setAttribute("class", "col-md-8 col-md-offset-2");
 
-var testMedia = document.createElement('div');
-testMedia.setAttribute("class", "media");
+for(var index = 0; index < 4; index++) {
+console.log(myPlaces[index]);
 
-var testMediaHeading = document.createElement('div');
-testMediaHeading.setAttribute("class", "media-left media-middle");
+var mediaInit = document.createElement('div');
+mediaInit.setAttribute("class", "media");
 
-var testLink = document.createElement('a');
-testLink.setAttribute("href", "restaurant.html");
+var mediaHeading = document.createElement('div');
+mediaHeading.setAttribute("class", "media-left media-middle");
 
-var testImg = document.createElement('img');
-testImg.setAttribute("class", "media-object");
-testImg.setAttribute("src", "images/yelp-logo.png");
+var link = document.createElement('a');
+link.setAttribute("href", "restaurant.html");
 
-testLink.appendChild(testImg);
-testMediaHeading.appendChild(testLink);
-testMedia.appendChild(testMediaHeading);
+var mediaImg = document.createElement('img');
+mediaImg.setAttribute("class", "media-object");
+mediaImg.setAttribute("src", "images/yelp-logo.png");
 
-var testMediaBody = document.createElement('div');
-testMediaBody.setAttribute("class", "media-body");
+link.appendChild(mediaImg);
+mediaHeading.appendChild(link);
+mediaInit.appendChild(mediaHeading);
+
+var mediaBody = document.createElement('div');
+mediaBody.setAttribute("class", "media-body");
 
 var testH3 = document.createElement('h3');
 testH3.setAttribute("class", "media-heading");
 testH3.setAttribute("id", "place");
 
-var testP = document.createElement('p');
-testP.setAttribute("id", "review1");
+mediaBody.appendChild(testH3);
 
-testMediaBody.appendChild(testH3);
-testMediaBody.appendChild(testP);
+for(var i = 1; i <= 5; i++) {
+  var mediaStar = document.createElement('span');
+  mediaStar.setAttribute("id", "reviewStar"+i);
 
-testMedia.appendChild(testMediaBody);
+  mediaBody.appendChild(mediaStar);
+}
 
-testPlace.appendChild(testMedia);
+var reviewPara = document.createElement('p');
+reviewPara.setAttribute("id", "review");
+
+mediaBody.appendChild(reviewPara);
+
+mediaInit.appendChild(mediaBody);
+
+mediaPlace.appendChild(mediaInit);
 //End generating media block
 
 position = document.getElementById('mediaBlock');
-position.appendChild(testMedia);
+position.appendChild(mediaInit);
 
 //Place name block
 var place = document.getElementById('place');
 var elPlace = document.createElement('span');
-var placeText = document.createTextNode(myPlaces[0].placeName);
+var placeText = document.createTextNode(myPlaces[index].placeName);
 
 elPlace.appendChild(placeText);
 place.appendChild(elPlace);
 // //End place name
 
+//Star block
+for (var i = 1; i <= myPlaces[index].star; i++) {
+  var position = document.getElementById('reviewStar'+i);
+  position.setAttribute("class", "fa fa-star");
+}
+//End Star block
+
 // Review block
-var inputReview = document.getElementById('review1');
+var inputReview = document.getElementById('review');
 var el = document.createElement('p');
-var elText = document.createTextNode(myPlaces[0].info);
+var elText = document.createTextNode(myPlaces[index].info);
 
 el.appendChild(elText);
 inputReview.appendChild(el);
 // End Review block
 
-console.log(testPlace);
+}
+
+console.log(mediaPlace);
