dfca17ade423342f7a3e0e8352d557a1a3069ebc
Justin Nguyen <jnguyen3950@gmail.com>
2016-04-14 11:14:01 -0700

    I added dynamic port.

--- http.js
+++ http.js
@@ -7,4 +7,7 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 });
 
-app.listen(8080);
+var port = process.env.PORT || 1337;
+app.listen(port, function() {
+ console.log("listening on port " + port);
+});
